6124578eceb36b26993ac10e967d6222cda55c82
azurize <46197517+azurize@users.noreply.github.com>
2023-06-01 08:21:24 -0700

    feat: add initial rank placeholder

---
+++ src/components/rank/Rank.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Rank = () => {
+  return (
+    <div>
+       <div className="light-blue f3">
+        {"PLACEHOLDER, your current rank is..."}
+       </div>
+       <div className="orange f1">
+        {"#5"}
+       </div>
+    </div>
+  );
+};
+
+export default Rank;
