d80bd680e2a670150a50e9989023b282f3885ff0
azurize <46197517+azurize@users.noreply.github.com>
2023-06-01 06:22:03 -0700

    fix: adjust button color

--- src/components/imageLinkForm/ImageLinkForm.js
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -10,7 +10,7 @@ const ImageLink = () => {
         <div className="center">
             <div className="center Console pa4 br3 shadow-5">
                 <input className="f4 pa2 w-70 center" type="text" />
-                <button className="w-30 grow f4 link ph3 pv2 dib white bg-gray">Detect!</button>
+                <button className="w-30 grow f4 link ph3 pv2 dib white bg-dark-gray">Detect!</button>
             </div>
         </div>
     </div>
