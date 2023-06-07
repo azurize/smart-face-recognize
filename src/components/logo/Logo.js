a2d5ac4a2466b834327521c6ff960f8a66fba5fb
David <46197517+azurize@users.noreply.github.com>
2023-06-07 10:06:43 -0700

    fix: center component

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -5,7 +5,7 @@ import "./Logo.css"
 
 const Logo = () => {
   return (
-    <div className="ma4 mt0">
+    <div className="center ma4 mt0">
       <Tilt className="Tilt br2 shadow-3" options={{ max: 25 }} style={{ height: 150, width: 150 }}>
         <div className="Tilt-inner pa3">
           <img style={{paddingTop: "25px"}} src={brain} alt="logo"/>
